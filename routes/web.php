ad32ae51ba2fbc234dc90e91f6e497b0f680c608
Anatolii <loomyy69@gmail.com>
2019-10-22 23:16:43 +0300

    some fixes

--- routes/web.php
+++ routes/web.php
@@ -11,23 +11,13 @@
 |
 */
 
-Route::resource('address/address','AddressController');
+Route::resource('/address','AddressController');
 
-Route::get('address/','AddressController@index')->name('address.index');
+Route::get('/','AddressController@index')->name('address.index');
 
-//Auth::routes();
+Auth::routes();
 
-Route::get('address/home', 'HomeController@index')->name('home');
+Route::get('/home', 'HomeController@index')->name('home');
 
-Route::get('address/show','AddressController@show')->name('address.show');
-Route::post('address/newLogin/{id}','AddressController@newLogin')->name('address.newLogin');
-
-
-// Authentication Routes...
-$this->get('address/login', 'Auth\LoginController@showLoginForm')->name('login');
-$this->post('address/login', 'Auth\LoginController@login');
-$this->post('address/logout', 'Auth\LoginController@logout')->name('logout');
-
-// Registration Routes...
-$this->get('address/register', 'Auth\RegisterController@showRegistrationForm')->name('register');
-$this->post('address/register', 'Auth\RegisterController@register');
+Route::get('/show','AddressController@show')->name('address.show');
+Route::post('/newLogin/{id}','AddressController@newLogin')->name('address.newLogin');
